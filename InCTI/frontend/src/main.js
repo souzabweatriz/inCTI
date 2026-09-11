de67193af26e2d0bf0c25617a92418cdf22b08ac
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2026-09-11 18:56:26 +0000

    refactor: page login inputs

--- InCTI/frontend/src/main.js
+++ InCTI/frontend/src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
